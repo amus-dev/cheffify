4f6a89b7e93079743bbc3c6080c2e938340defd6
Alexander Urrutia <matias04041994@gmail.com>
2024-01-25 13:13:44 -0300

    Seccion que comemos

---
+++ src/db/db.products.ts
@@ -0,0 +1,262 @@
+export const PRODUCTS = [
+  {
+    id: 1,
+    title: "Asado alemán con salsa mostaza",
+    description:
+      "Asado  jugoso y con carne de altísima calidad, salsa de mostaza liviana, le da un sabor especial, nos encanta",
+    category: "Carnes Rojas",
+    price: 4450,
+    alt: "Asado alemán con salsa mostaza - platos congelados",
+  },
+  {
+    id: 2,
+    title: "Lomo de cerdo con cebolla caramelizada",
+    description:
+      "Es de nuestros platos favoritos, tratamos a la cebolla con tanto cariño dejando solo ese sabor dulce y suave para acompañarlo en su perfecta cocción",
+    category: "Carnes Blancas",
+    price: 4450,
+    alt: "Lomo de cerdo con cebolla caramelizada - platos congelados",
+  },
+  {
+    id: 3,
+    title: "Albóndigas de carne",
+    description: "Receta familiar jugosa",
+    category: "Carnes Rojas",
+    price: 4450,
+    alt: "Albóndigas de carne - platos congelados",
+  },
+  {
+    id: 4,
+    title: "Strogonoff de carne",
+    description:
+      "Carne sin grasa y el corte es perfecto, la salsa lo acompaña muy bien, acompañalo con arroz blanco",
+    category: "Carnes Rojas",
+    price: 4450,
+    alt: "Strogonoff de carne - platos congelados",
+  },
+  {
+    id: 5,
+    title: "Pastel de choclo carne",
+    description:
+      "La receta es exacta a la de nuestra mamá, el choclo molido a la perfección y con un pino suave, la mezcla es mágica",
+    category: "Guisos",
+    price: 3950,
+    alt: "Pastel de choclo carne - platos congelados",
+  },
+  {
+    id: 6,
+    title: "Lasaña boloñesa",
+    description:
+      "Dirán esta lasaña la hace todo el mundo y es cierto, pero la nuestra es la más similar a la verdadera lasaña italiana",
+    category: "Masas",
+    price: 3950,
+    alt: "Lasaña boloñesa - platos congelados",
+  },
+  {
+    id: 7,
+    title: "Tomaticán",
+    description:
+      "Carne mechada con tomate fresco y choclo,  por supuesto que añadimos un toque de nuestro recetario familiar, maravilloso",
+    category: "Guisos",
+    price: 3950,
+    alt: "Tomaticán - platos congelados",
+  },
+  {
+    id: 8,
+    title: "Porotos granados",
+    description:
+      "Tienen una cocción particular de nuestro recetario familiar que dan ese sabor que te lleva a nuestro campo chileno, pruébalos  verás que sólo el olor te transporta",
+    category: "Guisos",
+    price: 3300,
+    alt: "Porotos granados - platos congelados",
+  },
+  {
+    id: 9,
+    title: "Pie de pollo",
+    description:
+      "La masa que hacemos en este pie es tan suave y delgada que puedes tener esa mezcla de sabores en tu boca del pollo jugoso y salteado",
+    category: "Masas",
+    price: 3950,
+    alt: "Pie de pollo - platos congelados",
+  },
+  {
+    id: 10,
+    title: "Lasaña pollo espinaca",
+    description:
+      "A nuestra espectacular lasaña de pollo le agregamos espinaca fresca, elegimos muy bien nuestras verduras, para que no se occiden en su cocción",
+    category: "Masas",
+    price: 3950,
+    alt: "Lasaña pollo espinaca - platos congelados",
+  },
+  {
+    id: 11,
+    title: "Quiche pollo pimentón",
+    description:
+      "A nuestra deliciosa quiche de pollo le quisimos agregar un poco de color y sabor con un toque de pimenton fresco",
+    category: "Masas",
+    price: 3950,
+    alt: "Quiche pollo pimentón - platos congelados",
+  },
+  {
+    id: 12,
+    title: "Tallarines panqueques con salsa pollo",
+    description:
+      "Cortamos y hacemos estos maravillosos tallarines con nuestros panqueques, nuestra salsa es de pollo natural",
+    category: "Masas",
+    price: 3950,
+    alt: "Tallarines panqueques con salsa pollo - platos congelados",
+  },
+  {
+    id: 13,
+    title: "Tallarines panqueques con salsa salmón",
+    description:
+      "Salmón que compramos para esta salsa es elegido personalmente, le damos el corte perfecto y la sazón para hacer una experiencia en tu boca",
+    category: "Masas",
+    price: 6450,
+    alt: "Tallarines panqueques con salsa salmón - platos congelados",
+  },
+  {
+    id: 14,
+    title: "Pollo con tocino",
+    description:
+      "Nos destacamos especialmente por la cocción de nuestros pollos, nuestro corte de pechuga deshuesada envuelta en fino tocino, quedando dorado por fuera y muy jugoso",
+    category: "Carnes Blancas",
+    price: 4450,
+    alt: "Pollo con tocino - platos congelados",
+  },
+  {
+    id: 15,
+    title: "Pollo arvejado",
+    description:
+      "Pollo con ese sabor tan particular de nuestras abuelas, la arveja tierna, con el corte de la zanahoria fina, y muy jugoso",
+    category: "Carnes Blancas",
+    price: 4450,
+    alt: "Pollo arvejado - platos congelados",
+  },
+  {
+    id: 16,
+    title: "Strogonoff de pollo",
+    description:
+      "Este strogonoff  es muy liviano, la salsa y el corte del pollo lo hace un plato suave y muy delicioso, con una sazón delicada que le da un gusto especial a la boca",
+    category: "Carnes Blancas",
+    price: 4450,
+    alt: "Strogonoff de pollo - platos congelados",
+  },
+  {
+    id: 17,
+    title: "Picante de camarón",
+    description:
+      "Es una receta exclusiva nuestra, de los platos más vendidos, son camarones ecuatorianos  de 36/40, con una salsa inigualable,  acompañalo con arroz blanco",
+    category: "Pescados y mariscos",
+    price: 7850,
+    alt: "Picante de camarón - platos congelados",
+  },
+  {
+    id: 18,
+    title: "Croquetas de atún",
+    description:
+      "Suave atún con aliños en su medida, quedan doradas por fuera y al partirlas veras el misterio de como quedan por dentro, atrévete",
+    category: "Pescados y mariscos",
+    price: 4450,
+    alt: "Croquetas de atún - platos congelados",
+  },
+  {
+    id: 20,
+    title: "Panqueques de pollo",
+    description:
+      "Nuestros exquisitos y únicos panqueques rellenos con pollo y con una suave salsa bechamel, te sirve para diferentes ocasiones y son muy livianos",
+    category: "Masas",
+    price: 3950,
+    alt: "Panqueques de pollo - platos congelados",
+  },
+  {
+    id: 21,
+    title: "Pastel de choclo pollo",
+    description:
+      "Al igual que el pastel de choclo carne, le hicimos esta variación en vez de carne le pusimos pollo, es muy suave y la combinación es perfecta, muy jugosa",
+    category: "Guisos",
+    price: 3950,
+    alt: "Pastel de choclo pollo - platos congelados",
+  },
+  {
+    id: 22,
+    title: "Quiche jamón queso",
+    description:
+      "Acompañala con un buen vino , la masa muy delgada, usamos queso muy suave, puedes trocearla para aperitivo o plato único",
+    category: "Masas",
+    price: 3950,
+    alt: "Quiche jamón queso - platos congelados",
+  },
+  {
+    id: 23,
+    title: "Quiche de Verdura",
+    description:
+      "Plato vegetariano, liviano con verduras frescas y masa muy delgada",
+    category: "Masas",
+    price: 3950,
+    alt: "Quiche de Verdura - platos congelados",
+  },
+  {
+    id: 23,
+    title: "Lasaña de pollo",
+    description:
+      "El secreto de nuestro recetario familiar está en la salsa bechamel, es única y le da ese toque más cremoso y suave, en cuanto a la masa, somos italianas ",
+    category: "Masas",
+    price: 3950,
+    alt: "Lasaña de pollo - platos congelados",
+  },
+  {
+    id: 24,
+    title: "Panqueques para rellenar (la docena)",
+    description:
+      "Delgados, suaves, te ayudan en cualquier comida dulce o salada, los mejores, en nuestro instagram te daremos muchas ideas para usarlos",
+    category: "Masas",
+    price: 3300,
+    alt: "Panqueques para rellenar (la docena) - platos congelados",
+  },
+  {
+    id: 25,
+    title: "Pastelera",
+    description:
+      "Esta pastelera es perfecta para acompañar alguna carne roja o blanca, está hecha con choclo tierno, le damos una sazón natural para que no pierda su sabor original",
+    category: "Masas",
+    price: 3300,
+    alt: "Pastelera - platos congelados",
+  },
+  {
+    id: 26,
+    title: "Crema de Zapallo",
+    description:
+      "Zapallo natural molido, le damos una cocción tan especial que se asoma un poco el dulzor  y cremosidad perfecta, pruebala invierno y verano",
+    category: "Cremas",
+    price: 3300,
+    alt: "Crema de Zapallo - platos congelados",
+  },
+  {
+    id: 27,
+    title: "Alfajor con chocolate",
+    description:
+      "Deliciosos alfajores de chocolate con suave relleno de manjar, el equilibrio perfecto entre dulzura y sabor que te hará desear más (8 Unidades).",
+    category: "Dulces",
+    price: 7900,
+    alt: "Alfajor con chocolate - comida prepadara",
+  },
+  {
+    id: 28,
+    title: "Galletas finas",
+    description:
+      "Galletas finas  crujientes por fuera, suaves por dentro. Saborea nuestros dulces sabores y disfruta de un momento ideal para tus tardes de té (1 KG Surtido).",
+    category: "Dulces",
+    price: 17000,
+    alt: "Galletas finas - comida prepadara",
+  },
+  {
+    id: 29,
+    title: "Pan de pascua",
+    description:
+      "El clásico pan de pascua, con su mezcla perfecta con almendras y nueces, especias y sabores navideños. Disfruta de su textura esponjosa y su aroma inigualable, en cada rebanada. (800 gr).",
+    category: "Dulces",
+    price: 10500,
+    alt: "Pan de pascua - comida prepadara",
+  },
+];
