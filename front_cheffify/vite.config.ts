ae1b515bb3ee51d94980e6164fdadf02e2616bd0
Alexander Urrutia <matias04041994@gmail.com>
2024-08-17 22:25:28 -0400

    Se configura nuevo directorio para rutas de secciones

--- front_cheffify/vite.config.ts
+++ front_cheffify/vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       "@components": path.resolve(__dirname, "./src/components"),
       "@assets": path.resolve(__dirname, "./src/assets"),
       "@utils": path.resolve(__dirname, "./src/utils"),
+      "@sections": path.resolve(__dirname, "./src/sections"),
     },
   },
 });
