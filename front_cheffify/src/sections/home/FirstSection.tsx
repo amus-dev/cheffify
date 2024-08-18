72e4d8daba4111d5043381493a8706bba13b5469
Alexander Urrutia <matias04041994@gmail.com>
2024-08-17 23:09:53 -0400

    Se agregan botones de seccion principal

--- front_cheffify/src/sections/home/FirstSection.tsx
+++ front_cheffify/src/sections/home/FirstSection.tsx
@@ -1,5 +1,6 @@
 import IconEmoteLove from "@assets/images/icons/icon-emote-love.svg";
 import ImagePrincipal from "@assets/images/img/principal-image.svg";
+import { Link } from "react-router-dom";
 
 const FirstSection = () => {
   return (
@@ -20,9 +21,24 @@ const FirstSection = () => {
             ¡Garantía de calidad!
             <img src={IconEmoteLove} className="size-5" />
           </strong>
-          <span></span>
         </p>
+        <div className="flex flex-col sm:flex-row items-center mt-10 gap-2 justify-center lg:justify-start">
+          <Link
+            to="/shop"
+            unstable_viewTransition
+            className="bg-secondary text-white font-bold w-full max-w-[240px] rounded-full text-[17px] text-center px-12 py-4 hover:bg-primary transition-all duration-500"
+          >
+            Hacer pedido
+          </Link>
+          <Link
+            to="/#comida"
+            className="border-secondary border-2 text-secondary font-bold w-full max-w-[240px] rounded-full text-[17px] text-center px-12 py-4 hover:border-primary hover:bg-primary hover:text-white transition-all duration-500"
+          >
+            Saber más
+          </Link>
+        </div>
       </div>
+
       <div className="w-full hidden lg:block">
         <img src={ImagePrincipal} className="w-full" />
       </div>
