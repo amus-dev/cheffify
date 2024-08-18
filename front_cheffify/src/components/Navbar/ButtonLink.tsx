fe8691d2380d3b01a21084296e8bfe5065b38105
Alexander Urrutia <matias04041994@gmail.com>
2024-08-17 21:26:42 -0400

    Se editan estilos

--- front_cheffify/src/components/Navbar/ButtonLink.tsx
+++ front_cheffify/src/components/Navbar/ButtonLink.tsx
@@ -9,14 +9,14 @@ interface ButtonLinkProps {
 const ButtonLink = ({ icon, alt, ariaLabel, text, url }: ButtonLinkProps) => {
   return (
     <a
-      className="text-primaryColor text-[15px] flex items-center gap-1 hover:scale-110 transition-all duration-500"
+      className="text-primary text-[14px] flex items-center gap-1 hover:scale-110 transition-all duration-500"
       href={url}
     >
       <img
         src={icon}
         alt={alt}
         aria-label={ariaLabel}
-        className="size-[14px]"
+        className="size-[13px]"
       />
       <span>{text}</span>
     </a>
