baf78afc7149bbe8d710c1a12551a73df517ddf4
Alexander Urrutia <matias04041994@gmail.com>
2024-08-19 10:54:38 -0400

    Se crea componente carousel

---
+++ front_cheffify/src/components/Carousels/CarouselHome.tsx
@@ -0,0 +1,23 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import { Pagination } from "swiper/modules";
+import "swiper/swiper-bundle.css";
+
+import Slide1 from "@assets/images/img/slide1.webp";
+import Slide2 from "@assets/images/img/slide2.webp";
+
+const CarouselHome = () => {
+  return (
+    <Swiper pagination={{ clickable: true }} modules={[Pagination]}>
+      <SwiperSlide>
+        <img src={Slide1} alt="Primera imagen del carousel" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={Slide2} alt="Segunda imagen del carousel" />
+      </SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default CarouselHome;
