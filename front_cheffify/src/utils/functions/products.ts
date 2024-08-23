297e56884024e8f8a094609d9dd5e9b1bf972b40
Alexander Urrutia <matias04041994@gmail.com>
2024-08-23 12:35:07 -0400

    Se crean funciones para productos

---
+++ front_cheffify/src/utils/functions/products.ts
@@ -0,0 +1,19 @@
+interface RandomArrayType<T> {
+  arr: Array<T>;
+  totalItems: number;
+}
+
+export const randomArray = <T>({
+  arr,
+  totalItems,
+}: RandomArrayType<T>): Array<T> => {
+  const result = arr.sort(() => Math.random() - 0.5).slice(0, totalItems);
+  return result;
+};
+
+export const formatPriceCLP = (price: number) => {
+  return new Intl.NumberFormat("es-CL", {
+    style: "currency",
+    currency: "CLP",
+  }).format(price);
+};
