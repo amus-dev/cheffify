bc40f37455beb63be1966dcaab7a5d86454d625e
Alexander Urrutia <matias04041994@gmail.com>
2024-08-23 12:34:24 -0400

    Se crea tipado de productos

---
+++ front_cheffify/src/utils/types/productTypes.ts
@@ -0,0 +1,22 @@
+export interface CardProductType {
+  id: number;
+  slug: string;
+  image: string;
+  title: string;
+  description: string;
+  price: number;
+  alt: string;
+}
+
+export interface Product {
+  id: number;
+  slug: string;
+  title: string;
+  description: string;
+  descriptionLarge: string;
+  metaDescription: string;
+  category: string;
+  price: number;
+  image: string;
+  alt: string;
+}
