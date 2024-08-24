7583950c1b8401f0d2e5d48c4447f4c44a83d03f
Alexander Urrutia <matias04041994@gmail.com>
2024-08-23 20:42:25 -0400

    Se agrega seccion en single product

--- front_cheffify/src/templates/TemplateShop.tsx
+++ front_cheffify/src/templates/TemplateShop.tsx
@@ -1,12 +1,14 @@
 import Footer from "@/components/common/footer";
 import MenuNav from "@/components/common/navbar/MenuNav";
 import Overlay from "@/components/common/overlay";
+import SecondSection from "@/components/sections/home/SecondSection";
 
 const TemplateShop = () => {
   return (
     <main>
       <MenuNav />
       <h1 className="text-primary">Single del producto</h1>
+      <SecondSection />
       <Overlay />
       <Footer />
     </main>
