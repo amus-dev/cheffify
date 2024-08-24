f28b58bb88028b80503c34a63d2579fbcf3873f7
Alexander Urrutia <matias04041994@gmail.com>
2024-08-23 20:40:18 -0400

    Se agrega overlay en congelados

--- front_cheffify/src/templates/TemplateCongelados.tsx
+++ front_cheffify/src/templates/TemplateCongelados.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/common/footer";
 import MenuNav from "@/components/common/navbar/MenuNav";
+import Overlay from "@/components/common/overlay";
 import Banner from "@/components/sections/congelados/Banner";
 import ComoPreparar from "@/components/sections/congelados/ComoPreparar";
 import Ventajas from "@/components/sections/congelados/Ventajas";
@@ -11,6 +12,7 @@ const TemplateCongelados = () => {
       <Banner />
       <Ventajas />
       <ComoPreparar />
+      <Overlay />
       <Footer />
     </main>
   );
