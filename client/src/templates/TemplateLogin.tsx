c0fc10d7d702088ac4bab78b79bf6c470eb66fc5
Alexander Urrutia <matias04041994@gmail.com>
2024-08-26 17:26:57 -0400

    Se agrega toast en el template

--- client/src/templates/TemplateLogin.tsx
+++ client/src/templates/TemplateLogin.tsx
@@ -2,12 +2,16 @@ import Footer from "@/components/common/footer";
 import MenuNav from "@/components/common/navbar/MenuNav";
 import Overlay from "@/components/common/overlay";
 import LoginForm from "@/components/Session/LoginForm";
+import { Toaster } from "sonner";
 
 const TemplateLogin = () => {
   return (
     <main>
       <MenuNav />
-      <LoginForm />
+      <section className="flex justify-center my-20">
+        <LoginForm />
+      </section>
+      <Toaster />
       <Footer />
       <Overlay />
     </main>
