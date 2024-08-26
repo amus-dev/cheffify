ce9938631634d487f6f3438a1f0eb5f3480cb896
Alexander Urrutia <matias04041994@gmail.com>
2024-08-26 18:27:40 -0400

    Proteger ruta perfil solo cuando existe el token

--- client/src/pages/Perfil.tsx
+++ client/src/pages/Perfil.tsx
@@ -1,6 +1,12 @@
 import TemplatePerfil from "@/templates/TemplatePerfil";
+import { Navigate } from "react-router-dom";
 
 const Perfil = () => {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    return <Navigate to="/login" />;
+  }
+
   return <TemplatePerfil />;
 };
 
