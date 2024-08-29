c5f981cbc9b7328d4f99ae0c28bf60e6a6df2c81
Alexander Urrutia <matias04041994@gmail.com>
2024-08-28 20:24:11 -0400

    chore: Update toast style for success and error messages

--- client/src/utils/functions/showToast.tsx
+++ client/src/utils/functions/showToast.tsx
@@ -13,12 +13,16 @@ export const showToast = ({
   duration = 3000,
   type,
 }: ShowToastProps) => {
+  const toastStyle = {
+    border: "1px solid #D77466",
+  };
+
   const toastContent = (
-    <div className="flex items-center justify-center gap-3">
+    <div className="flex items-center justify-center gap-3 p-0 ">
       {icon && <img src={icon} alt={`${type} icon`} />}
       <span
         className={`text-sm font-bold ${
-          type === "success" ? "text-primary" : "text-red-600"
+          type === "success" ? "text-primary" : "text-secondary"
         }`}
       >
         {message}
@@ -27,8 +31,8 @@ export const showToast = ({
   );
 
   if (type === "success") {
-    return toast.success(toastContent, { duration });
+    return toast.success(toastContent, { duration, style: toastStyle });
   } else {
-    return toast.error(toastContent, { duration });
+    return toast.error(toastContent, { duration, style: toastStyle });
   }
 };
