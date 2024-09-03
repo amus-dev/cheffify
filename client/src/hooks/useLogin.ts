101655e38a6470a1af6b126165ebec1e6c0ebbc7
Alexander Urrutia <matias04041994@gmail.com>
2024-09-03 18:08:50 -0400

    refactor: Update useLogin hook to use useFetch for API requests and handle token authentication

--- client/src/hooks/useLogin.ts
+++ client/src/hooks/useLogin.ts
@@ -1,33 +1,27 @@
-import IconCheck from "@/assets/images/icons/check.svg";
-import { navigateWithViewTransition } from "@/utils/functions/navigate";
-import { showToast } from "@/utils/functions/showToast";
+import { useFetch } from "@/hooks/useFetch";
 import { LoginFormType } from "@/utils/types/formTypes";
-import { useState } from "react";
 import { NavigateFunction } from "react-router-dom";
+import { navigateWithViewTransition } from "@/utils/functions/navigate";
+import IconCheck from "@/assets/images/icons/check.svg";
+import { showToast } from "@/utils/functions/showToast";
 
 export const useLogin = () => {
-  const [loading, setLoading] = useState(false);
+  const { fetchData, loading } = useFetch();
 
-  const loginSubmitHandler = async (
+  const loginSubmitHandler = (
     data: LoginFormType,
     navigate: NavigateFunction
   ) => {
-    setLoading(true);
-    try {
-      const result = await fetch(
-        `${import.meta.env.VITE_API_URL}?action=login`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(data),
-        }
-      );
-
-      const response = await result.json();
-
-      if (result.ok) {
+    fetchData({
+      url: `${import.meta.env.VITE_API_URL}?action=login`,
+      options: {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      },
+      onSuccess: (response) => {
         localStorage.setItem("token", response.data.token);
         showToast({
           message: response.message,
@@ -36,15 +30,9 @@ export const useLogin = () => {
         });
         setTimeout(() => {
           navigateWithViewTransition(navigate, "/perfil");
-          setLoading(false);
         }, 3000);
-      } else {
-        showToast({ message: response.message, type: "error" });
-      }
-    } catch (error) {
-      console.error("Error en el login:", error);
-      showToast({ message: "Ocurrió un error inesperado", type: "error" });
-    }
+      },
+    });
   };
 
   return { loginSubmitHandler, loading };
