82766eb97ee5fd05c966ec2872ceb31b0778bafd
Alexander Urrutia <matias04041994@gmail.com>
2024-09-03 18:10:33 -0400

    refactor: Update useCreateAccount and useUpdatePhone hooks to use useFetch for API requests

--- client/src/hooks/useCreateAccount.ts
+++ client/src/hooks/useCreateAccount.ts
@@ -1,32 +1,27 @@
-import IconCheck from "@/assets/images/icons/check.svg";
-import { navigateWithViewTransition } from "@/utils/functions/navigate";
-import { showToast } from "@/utils/functions/showToast";
+import { useFetch } from "@/hooks/useFetch";
 import { AccountFormType } from "@/utils/types/formTypes";
-import { useState } from "react";
 import { NavigateFunction } from "react-router-dom";
+import { navigateWithViewTransition } from "@/utils/functions/navigate";
+import IconCheck from "@/assets/images/icons/check.svg";
+import { showToast } from "@/utils/functions/showToast";
 
 export const useCreateAccount = () => {
-  const [loading, setLoading] = useState(false);
+  const { fetchData, loading } = useFetch();
 
-  const accountSubmitHandler = async (
+  const accountSubmitHandler = (
     data: AccountFormType,
     navigate: NavigateFunction
   ) => {
-    setLoading(true);
-    try {
-      const result = await fetch(
-        `${import.meta.env.VITE_API_URL}?action=create`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(data),
-        }
-      );
-
-      const response = await result.json();
-      if (result.ok) {
+    fetchData({
+      url: `${import.meta.env.VITE_API_URL}?action=create`,
+      options: {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      },
+      onSuccess: (response) => {
         showToast({
           message: response.message,
           type: "success",
@@ -34,17 +29,9 @@ export const useCreateAccount = () => {
         });
         setTimeout(() => {
           navigateWithViewTransition(navigate, "/login");
-          setLoading(false);
         }, 3000);
-      } else {
-        setLoading(false);
-        showToast({ message: response.message, type: "error" });
-      }
-    } catch (error) {
-      console.error("Error en el login:", error);
-      setLoading(false);
-      showToast({ message: "Ocurrió un error inesperado", type: "error" });
-    }
+      },
+    });
   };
 
   return { accountSubmitHandler, loading };
--- client/src/hooks/useUpdatePhone.ts
+++ client/src/hooks/useUpdatePhone.ts
@@ -1,43 +1,29 @@
+import { useFetch } from "@/hooks/useFetch";
+import { UpdatePhoneType } from "@/utils/types/formTypes";
 import IconCheck from "@/assets/images/icons/check.svg";
 import { showToast } from "@/utils/functions/showToast";
-import { UpdatePhoneType } from "@/utils/types/formTypes";
-import { useState } from "react";
 
 export const useUpdatePhone = () => {
-  const [loading, setLoading] = useState(false);
+  const { fetchData, loading } = useFetch();
 
-  const updatePhoneHandler = async (data: UpdatePhoneType) => {
-    setLoading(true);
-    try {
-      const result = await fetch(
-        `${import.meta.env.VITE_API_URL}?action=updatePhone`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(data),
-        }
-      );
-
-      const response = await result.json();
-
-      if (result.ok) {
+  const updatePhoneHandler = (data: UpdatePhoneType) => {
+    fetchData({
+      url: `${import.meta.env.VITE_API_URL}?action=updatePhone`,
+      options: {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      },
+      onSuccess: (response) => {
         showToast({
           message: response.message,
           type: "success",
           icon: IconCheck,
         });
-        setTimeout(() => {
-          setLoading(false);
-        }, 3000);
-      } else {
-        showToast({ message: response.message, type: "error" });
-      }
-    } catch (error) {
-      console.error("Error en el login:", error);
-      showToast({ message: "Ocurrió un error inesperado", type: "error" });
-    }
+      },
+    });
   };
 
   return { updatePhoneHandler, loading };
