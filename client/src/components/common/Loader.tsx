85219f131ca9c46c9d9d433d63618fc3c5142474
Alexander Urrutia <matias04041994@gmail.com>
2024-09-03 12:52:13 -0400

    chore: Add Loader component for displaying loading animation

---
+++ client/src/components/common/Loader.tsx
@@ -0,0 +1,7 @@
+const Loader = () => {
+  return (
+    <div className="w-9 h-9 border-4 border-solid border-gray-300 border-t-secondary rounded-full animate-spin"></div>
+  );
+};
+
+export default Loader;
