91aff9245d11f625e507fe3b4deaa956d27e038b
Alexander Urrutia <matias04041994@gmail.com>
2024-09-03 12:59:21 -0400

    chore: Refactor API route handling to use switch statement for better readability

--- server/src/routes/api.php
+++ server/src/routes/api.php
@@ -3,9 +3,19 @@ require_once '../controllers/AuthController.php';
 
 $authController = new AuthController();
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_GET['action'] === 'login') {
-     $data = json_decode(file_get_contents("php://input"), true);
-     $authController->login($data);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+     switch ($_GET['action']) {
+          case 'login':
+               $data = json_decode(file_get_contents("php://input"), true);
+               $authController->login($data);
+               break;
+          default:
+               ResponseHelper::sendResponse(404, "Route not found");
+               break;
+     }
 } else {
      ResponseHelper::sendResponse(404, "Route not found");
 }
+
+
