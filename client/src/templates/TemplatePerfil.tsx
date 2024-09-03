4b10584ef489905bcd89918335f3e73f3b077e93
Alexander Urrutia <matias04041994@gmail.com>
2024-09-03 17:51:01 -0400

    refactor: Update TemplatePerfil to handle user authentication and navigation

--- client/src/templates/TemplatePerfil.tsx
+++ client/src/templates/TemplatePerfil.tsx
@@ -2,9 +2,16 @@ import Footer from "@/components/common/footer";
 import MenuNav from "@/components/common/navbar/MenuNav";
 import Overlay from "@/components/common/overlay";
 import HeaderInfo from "@/components/Profile/HeaderInfo";
+import { useToken } from "@/hooks/useToken";
+import { useNavigate } from "react-router-dom";
 import { Toaster } from "sonner";
 
 const TemplatePerfil = () => {
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+  const { tokenHandler } = useToken();
+  tokenHandler({ token }, navigate);
+
   return (
     <main>
       <MenuNav />
