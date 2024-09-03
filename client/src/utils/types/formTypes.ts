c6009486e153e3fd6c06616ca914cb6d7c17eff8
Alexander Urrutia <matias04041994@gmail.com>
2024-09-03 15:52:39 -0400

    refactor: Add LoginFormType and AccountFormType interfaces for form validation and submission

---
+++ client/src/utils/types/formTypes.ts
@@ -0,0 +1,13 @@
+export interface LoginFormType {
+  email: string;
+  password: string;
+}
+
+export interface AccountFormType {
+  email: string;
+  name: string;
+  lastName: string;
+  phone: number;
+  password: string;
+  confirmPassword: string;
+}
