b2cd81eb99c751539947ac63f64ad62f5bd5b5ac
Alexander Urrutia <matias04041994@gmail.com>
2024-09-08 16:49:13 -0300

    feat: Add new carousel slide for Porotos with Tomate icon

--- client/src/config/data.carousels.ts
+++ client/src/config/data.carousels.ts
@@ -2,6 +2,8 @@ import SlideArroz from "@/assets/images/img/slide-arroz.webp";
 import IconArroz from "@/assets/images/icons/iconos_arroz_slider.svg";
 import SlideCrema from "@/assets/images/img/slideCrema.webp";
 import IconTallarin from "@/assets/images/icons/icon-tallarines.svg";
+import SlidePorotos from "@/assets/images/img/slidePorotos.webp";
+import IconTomate from "@/assets/images/icons/icon-tomate.svg";
 
 export const dataCarouselsShop = [
   {
@@ -33,5 +35,20 @@ export const dataCarouselsShop = [
     buttonTextColorHover: "text-white",
     link: "",
     color: "text-primary",
+  },
+  {
+    id: 3,
+    title: "Comida casera directamente a tu mesa",
+    description:
+      "La vistosidad y atractivo de nuestros platos radica en el color de sus ingredientes, cuidadosamente seleccionados para lograr en nuestras recetas un penetrante olor y sabor inigualables.",
+    image: SlidePorotos,
+    icon: IconTomate,
+    buttonText: "¡Me dio hambre!",
+    buttonBackground: "bg-secondary",
+    buttonBackgroundHover: "bg-green",
+    buttonTextColor: "text-white",
+    buttonTextColorHover: "text-white",
+    link: "",
+    color: "text-white",
   },
 ];
