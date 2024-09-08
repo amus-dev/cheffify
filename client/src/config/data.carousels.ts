a737bb4a2ec85db7295e2e5b83bc074475a5a47f
Alexander Urrutia <matias04041994@gmail.com>
2024-09-08 16:38:03 -0300

    refactor: Add data for carousels in Shop page

---
+++ client/src/config/data.carousels.ts
@@ -0,0 +1,27 @@
+import SlideArroz from "@/assets/images/img/slide-arroz.webp";
+import IconArroz from "@/assets/images/icons/iconos_arroz_slider.svg";
+
+export const dataCarouselsShop = [
+  {
+    id: 1,
+    title: "Sabores del mundo en arroz",
+    description:
+      "Descubre el auténtico sabor de los arroces internacionales, conservados a la perfección con la mejor técnica de congelación y entregados directamente a tu mesa a través de nuestro eficaz delivery.",
+    image: SlideArroz,
+    icon: IconArroz,
+    buttonText: "¡A comer!",
+    link: "",
+    color: "text-lightBeige",
+  },
+  {
+    id: 2,
+    title: "Sabores del mundo en arroz",
+    description:
+      "Descubre el auténtico sabor de los arroces internacionales, conservados a la perfección con la mejor técnica de congelación y entregados directamente a tu mesa a través de nuestro eficaz delivery.",
+    image: SlideArroz,
+    icon: IconArroz,
+    buttonText: "¡A comer!",
+    link: "",
+    color: "text-lightBeige",
+  },
+];
