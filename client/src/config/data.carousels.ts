2ffa6ace2e848d19f32386eea2d333ab64fd75b4
Alexander Urrutia <matias04041994@gmail.com>
2024-09-08 16:45:33 -0300

    refactor: Update carousels data and styles in Shop page

--- client/src/config/data.carousels.ts
+++ client/src/config/data.carousels.ts
@@ -1,5 +1,7 @@
 import SlideArroz from "@/assets/images/img/slide-arroz.webp";
 import IconArroz from "@/assets/images/icons/iconos_arroz_slider.svg";
+import SlideCrema from "@/assets/images/img/slideCrema.webp";
+import IconTallarin from "@/assets/images/icons/icon-tallarines.svg";
 
 export const dataCarouselsShop = [
   {
@@ -10,18 +12,26 @@ export const dataCarouselsShop = [
     image: SlideArroz,
     icon: IconArroz,
     buttonText: "¡A comer!",
+    buttonBackground: "bg-secondary",
+    buttonBackgroundHover: "bg-green",
+    buttonTextColor: "text-white",
+    buttonTextColorHover: "text-white",
     link: "",
     color: "text-lightBeige",
   },
   {
     id: 2,
-    title: "Sabores del mundo en arroz",
+    title: "Platos preparados a domicilio",
     description:
-      "Descubre el auténtico sabor de los arroces internacionales, conservados a la perfección con la mejor técnica de congelación y entregados directamente a tu mesa a través de nuestro eficaz delivery.",
-    image: SlideArroz,
-    icon: IconArroz,
+      "Platos caseros que llegan directamente a tu mesa con todas sus propiedades y con el frescor de su sabor natural gracias a un eficiente proceso de congelado y cadena de distribución.",
+    image: SlideCrema,
+    icon: IconTallarin,
     buttonText: "¡A comer!",
+    buttonBackground: "bg-white",
+    buttonBackgroundHover: "bg-green",
+    buttonTextColor: "text-primary",
+    buttonTextColorHover: "text-white",
     link: "",
-    color: "text-lightBeige",
+    color: "text-primary",
   },
 ];
