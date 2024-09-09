e9f37f1f8a9ce9b75ad470a9db7ed06e25c161d2
Alexander Urrutia <matias04041994@gmail.com>
2024-09-09 19:10:52 -0300

    feat: Add new category icons
    Add new category icons for "Carnes Blancas", "Masas", "Pescados y mariscos", "Cremas", and "Arroces" in the ProductsShop component.

--- client/src/config/data.category.products.ts
+++ client/src/config/data.category.products.ts
@@ -2,6 +2,11 @@ import { CategoryProduct } from "@/utils/types/productTypes";
 
 import IconMeat from "@/assets/images/icons/icon-meat.svg";
 import IconGuiso from "@/assets/images/icons/icon-cooking-pot.svg";
+import IconCarnesBlancas from "@/assets/images/icons/icon-chicken.svg";
+import IconMasas from "@/assets/images/icons/icon-masas.svg";
+import IconFish from "@/assets/images/icons/icon-fish.svg";
+import IconCremas from "@/assets/images/icons/icon-cremas.svg";
+import IconArroz from "@/assets/images/icons/icon-arroces.svg";
 
 export const CATEGORIES: CategoryProduct[] = [
   {
@@ -16,6 +21,41 @@ export const CATEGORIES: CategoryProduct[] = [
     name: "Guisos",
     slug: "guisos",
     icon: IconGuiso,
+    active: false,
+  },
+  {
+    id: 3,
+    name: "Carnes Blancas",
+    slug: "carnes-blancas",
+    icon: IconCarnesBlancas,
+    active: false,
+  },
+  {
+    id: 4,
+    name: "Masas",
+    slug: "masas",
+    icon: IconMasas,
+    active: false,
+  },
+  {
+    id: 4,
+    name: "Pescados y mariscos",
+    slug: "pescados-y-mariscos",
+    icon: IconFish,
+    active: false,
+  },
+  {
+    id: 5,
+    name: "Cremas",
+    slug: "cremas",
+    icon: IconCremas,
+    active: false,
+  },
+  {
+    id: 5,
+    name: "Arroces",
+    slug: "arroces",
+    icon: IconArroz,
     active: false,
   },
 ];
