a8fd1b096d17020b9288bce9836fa8a95b12cb9e
Alexander Urrutia <matias04041994@gmail.com>
2024-09-19 23:33:39 -0300

    Refactor MenuNav component to improve scroll behavior and add animation

--- client/src/components/common/navbar/MenuNav.tsx
+++ client/src/components/common/navbar/MenuNav.tsx
@@ -7,22 +7,28 @@ import CartProducts from "@/components/Products/CartProducts";
 import { MENU_ITEMS } from "@/utils/const/menu";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
-declare module "react-router-dom" {
-  interface LinkProps {
-    unstable_viewTransition?: boolean;
-  }
-}
+import { motion, useAnimation } from "framer-motion";
 
 const MenuNav = () => {
   const [hasScrolled, setHasScrolled] = useState(false);
+  const controls = useAnimation();
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 0) {
+      if (window.scrollY > 50) {
         setHasScrolled(true);
+        controls.start({
+          y: 0, // Menú en posición fixed
+          opacity: 1,
+          transition: { duration: 0.3 },
+        });
       } else {
         setHasScrolled(false);
+        controls.start({
+          y: 0, // Mantiene el menú visible
+          opacity: 1, // Mantiene la opacidad visible
+          transition: { duration: 0.3 },
+        });
       }
     };
 
@@ -31,12 +37,14 @@ const MenuNav = () => {
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, [controls]);
 
   return (
-    <nav
-      className={`top-0 left-0 right-0 z-20 px-[20px] lg:px-[60px] py-[20px] xl:px-[170px] flex items-center justify-between bg-white transition-shadow duration-500 ${
-        hasScrolled ? "shadow-lg !z-50 !fixed" : "relative"
+    <motion.nav
+      initial={{ y: 0, opacity: 1 }} // Al cargar la página, siempre visible
+      animate={controls} // Controla la animación al hacer scroll
+      className={`fixed top-0 left-0 right-0 px-[20px] lg:px-[60px] py-[20px] xl:px-[170px] flex items-center justify-between bg-white transition-shadow duration-500 ${
+        hasScrolled ? "shadow-lg z-30" : ""
       }`}
     >
       <div>
@@ -45,13 +53,15 @@ const MenuNav = () => {
           unstable_viewTransition
           aria-label="Logo de Cheffify que lleva a la home"
         >
-          <img
-            className="w-full max-w-[115px] hover:scale-110 transition-all duration-500"
+          <motion.img
+            whileHover={{ scale: 1.1 }}
+            className="w-full max-w-[115px] transition-all duration-500"
             src={Logo}
             alt="Logo del menu navegación Cheffify"
           />
         </Link>
       </div>
+
       <ul className="md:flex gap-10 hidden">
         {MENU_ITEMS.map(({ icon, alt, ariaLabel, text, url, ancla }, index) => (
           <li key={index}>
@@ -66,13 +76,14 @@ const MenuNav = () => {
           </li>
         ))}
       </ul>
+
       <div className="flex items-center gap-2">
         <FoodButton />
         <AccountButton />
         <LogOutButton />
       </div>
       <CartProducts />
-    </nav>
+    </motion.nav>
   );
 };
 
