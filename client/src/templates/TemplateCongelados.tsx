4481cbf8d2730d58c77559b45e760300febcd319
Alexander Urrutia <matias04041994@gmail.com>
2024-09-20 00:08:00 -0300

    Refactor TemplateCongelados component to scroll to top on page load

--- client/src/templates/TemplateCongelados.tsx
+++ client/src/templates/TemplateCongelados.tsx
@@ -4,8 +4,13 @@ import Overlay from "@/components/common/overlay";
 import Banner from "@/components/sections/congelados/Banner";
 import ComoPreparar from "@/components/sections/congelados/ComoPreparar";
 import Ventajas from "@/components/sections/congelados/Ventajas";
+import { scrollToTop } from "@/utils/functions/navigate";
+import { useEffect } from "react";
 
 const TemplateCongelados = () => {
+  useEffect(() => {
+    scrollToTop();
+  }, []);
   return (
     <main className="mt-28">
       <MenuNav />
