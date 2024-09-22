2ef748d44e0371bdf392d733e93e8946994beac9
Alexander Urrutia <matias04041994@gmail.com>
2024-09-22 14:37:35 -0300

    Refactor TemplateResponseWebpay to handle Webpay response and display message

--- client/src/templates/TemplateResponseWebpay.tsx
+++ client/src/templates/TemplateResponseWebpay.tsx
@@ -1,10 +1,32 @@
 import Footer from "@/components/common/footer";
 import MenuNav from "@/components/common/navbar/MenuNav";
+import { usePayTransbank } from "@/hooks/usePayTransbank";
+import { useEffect } from "react";
 
 const TemplateResponseWebpay = () => {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+
+  const { getResponseWebpay, messageResponseWebpay } = usePayTransbank();
+  useEffect(() => {
+    // Crear un objeto vacío para almacenar todos los parámetros
+    const params: { [key: string]: string } = {};
+    // Iterar sobre cada parámetro y agregarlo al objeto
+    urlParams.forEach((value, key) => {
+      params[key] = value;
+    });
+    getResponseWebpay(params);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <main className="mt-28">
+    <main>
       <MenuNav />
+      <div className="px-[20px] lg:px-[60px] xl:px-[170px] mt-40 py-20">
+        <h1 className="text-secondary text-4xl font-extrabold">
+          {messageResponseWebpay && messageResponseWebpay}
+        </h1>
+      </div>
       <Footer />
     </main>
   );
