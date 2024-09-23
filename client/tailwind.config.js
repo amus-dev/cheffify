693f37ac267c9fee970e2f220bdfd2b75e63319c
Alexander Urrutia <matias04041994@gmail.com>
2024-09-23 17:09:23 -0300

    Refactor MisDirecciones component to include address selection modal

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -20,6 +20,21 @@ module.exports = {
         "card-shadow": "rgba(215, 116, 102, 0.4) 0px 0px 15px",
         arrow: "rgba(215, 116, 102, 0.4) 0px 0px 15px",
       },
+      keyframes: {
+        "fade-in": {
+          "0%": {
+            opacity: 0,
+            transform: "scale(0.95)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "scale(1)",
+          },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in 0.3s ease-out forwards",
+      },
     },
   },
   plugins: [],
