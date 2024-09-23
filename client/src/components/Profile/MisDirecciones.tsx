b8542d072e886829e234f2ef39efb9c7aa62e6f0
Alexander Urrutia <matias04041994@gmail.com>
2024-09-23 16:40:34 -0300

    Refactor MisDirecciones component to include an "Agregar dirección" button and handle empty address case

--- client/src/components/Profile/MisDirecciones.tsx
+++ client/src/components/Profile/MisDirecciones.tsx
@@ -1,9 +1,16 @@
 const MisDirecciones = () => {
+  const EMPTY_ADDRESS = true;
+
   return (
-    <section>
-      <p className="text-secondary font-extrabold text-lg text-center">
-        No se han registrado direcciones
-      </p>
+    <section className="flex flex-col items-center justify-center">
+      <button className="flex items-center justify-center gap-4 bg-secondary w-fit rounded-[5px] text-[15px] transition-all duration-500 hover:bg-primary py-4 px-10 font-extrabold mb-4">
+        Agregar dirección
+      </button>
+      {EMPTY_ADDRESS && (
+        <p className="text-secondary font-extrabold text-lg text-center">
+          No se han registrado direcciones
+        </p>
+      )}
     </section>
   );
 };
