ecdb57a79818e5a95f431f8cd72824fa1c106d8b
Alexander Urrutia <matias04041994@gmail.com>
2024-09-23 17:24:10 -0300

    Refactor ModalAddress component to include form validation and submission

--- client/src/components/common/modals/ModalAddress.tsx
+++ client/src/components/common/modals/ModalAddress.tsx
@@ -1,8 +1,27 @@
+import { COMUNAS } from "@/utils/const/address";
+import { SubmitHandler, useForm } from "react-hook-form";
+
 interface ModalAddressProps {
   setIsOpen: (value: boolean) => void;
 }
 
+type Inputs = {
+  name: string;
+  address: string;
+  comuna: string;
+};
+
 const ModalAddress = ({ setIsOpen }: ModalAddressProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    console.log(data);
+  };
+
   return (
     <div
       className="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center z-50 transition-opacity duration-300 ease-out"
@@ -12,14 +31,69 @@ const ModalAddress = ({ setIsOpen }: ModalAddressProps) => {
         className="bg-white p-8 rounded-lg shadow-lg relative animate-fade-in"
         onClick={(e) => e.stopPropagation()} // Evitar cerrar al hacer clic dentro del modal
       >
-        <h2 className="text-2xl font-bold mb-4">This is a Modal</h2>
-        <p className="mb-4">You can add any content you like here.</p>
-        <button
-          onClick={() => setIsOpen(false)}
-          className="px-4 py-2 bg-red-500 text-white font-semibold rounded hover:bg-red-600"
+        <div className="flex items-center justify-between mb-4">
+          <h2 className="text-xl font-bold text-primary">Datos de dirección</h2>
+          <button
+            className="text-secondary font-extrabold text-lg transition-colors duration-500 hover:text-primary"
+            onClick={() => setIsOpen(false)}
+          >
+            X
+          </button>
+        </div>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="space-y-4 w-full max-w-lg"
         >
-          Close
-        </button>
+          <input
+            id="name"
+            type="name"
+            {...register("name", {
+              required: "El nombre es requerido",
+            })}
+            className="bg-bgInput text-colorInput font-extrabold text-[13px] p-2 w-full rounded-md h-[46px]"
+            placeholder="Email"
+            autoComplete="email"
+          />
+          {errors.name && (
+            <span className="text-red-600 text-sm">{errors.name.message}</span>
+          )}
+          <input
+            id="address"
+            type="address"
+            {...register("address", {
+              required: "El nombre es requerido",
+            })}
+            className="bg-bgInput text-colorInput font-extrabold text-[13px] p-2 w-full rounded-md h-[46px]"
+            placeholder="Email"
+            autoComplete="email"
+          />
+          {errors.address && (
+            <span className="text-red-600 text-sm">
+              {errors.address.message}
+            </span>
+          )}
+          <select
+            id="comuna"
+            className="bg-bgInput text-colorInput font-bold text-[13px] p-2 w-full rounded-md h-[46px]"
+            {...register("comuna", { required: "La comuna es requerida" })}
+          >
+            {COMUNAS.map((comuna, index) => (
+              <option key={index} value={comuna.value}>
+                {comuna.label}
+              </option>
+            ))}
+          </select>
+          {errors.comuna && (
+            <span className="text-red-600 text-sm">
+              {errors.comuna.message}
+            </span>
+          )}
+          <input
+            type="submit"
+            value="Guardar dirección"
+            className="bg-secondary text-white font-bold w-full rounded-md text-[17px] px-12 py-4 hover:bg-primary transition-all duration-500 cursor-pointer"
+          />
+        </form>
       </div>
     </div>
   );
