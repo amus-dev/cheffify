fdf94a84e479459990c75ecf6d84607861971b22
Alexander Urrutia <matias04041994@gmail.com>
2024-09-25 15:59:19 -0300

    Refactor userStore.ts to include address functionality

--- client/src/stores/userStore.ts
+++ client/src/stores/userStore.ts
@@ -1,16 +1,20 @@
 import { create } from "zustand";
-import { UserType } from "@/utils/types/formTypes";
+import { Address, UserType } from "@/utils/types/formTypes";
 
 interface UserStore {
   user: UserType | null;
   setUserStore: (user: UserType) => void;
   clearUserStore: () => void;
+  address: Address[] | null;
+  setAddressStore: (address: Address[]) => void;
 }
 
 const useUserStore = create<UserStore>((set) => ({
   user: null,
   setUserStore: (user) => set({ user }),
   clearUserStore: () => set({ user: null }),
+  address: null,
+  setAddressStore: (address) => set({ address }),
 }));
 
 export default useUserStore;
